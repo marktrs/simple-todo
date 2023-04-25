598ac1e77b9c14a78109656ab55ca8520f0c8915
marktrs <traisak.tra@gmail.com>
2023-04-26 03:13:52 +0700

    Added logger for config loader error

--- config/config.go
+++ config/config.go
@@ -1,18 +1,21 @@
 package config
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/marktrs/simple-todo/logger"
 )
 
 // Config func to get env value
 func Config(key string) string {
+	log := logger.Log
+
 	// load .env file
 	err := godotenv.Load(".env")
 	if err != nil {
-		fmt.Print("Error loading .env file")
+		log.Warn().AnErr("error", err).Msg("Error loading .env file, using system env")
 	}
+
 	return os.Getenv(key)
 }
